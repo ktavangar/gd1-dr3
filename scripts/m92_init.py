2d1da75a1c8b81cb457dfb3d7ab957b6eaf35bdf
Kiyan Tavangar <ktavangar99@gmail.com>
2024-11-06 09:20:00 -0500

    updates pre-new-api

--- scripts/m92_init.py
+++ scripts/m92_init.py
@@ -15,17 +15,17 @@ from gd1_helpers.membership.gd1_model import StreamDensModel
 
 def setup_models(cls, pawprint, data):
     cls.phi1_lim = (np.min(data['phi1']), np.max(data['phi1']))
-    phi2_lim = (np.min(data['phi2']), np.max(data['phi2']))
+    cls.phi2_lim = (np.min(data['phi2']), np.max(data['phi2']))
     pm1_lim = (np.min(pawprint.pmprint.vertices[:,0]), np.max(pawprint.pmprint.vertices[:,0]))
     pm2_lim = (np.min(pawprint.pmprint.vertices[:,1]), np.max(pawprint.pmprint.vertices[:,1]))
 
-    cls.coord_bounds = {"phi1": cls.phi1_lim, "phi2": phi2_lim, "pm1": pm1_lim, "pm2": pm2_lim}
+    cls.coord_bounds = {"phi1": cls.phi1_lim, "phi2": cls.phi2_lim, "pm1": pm1_lim, "pm2": pm2_lim}
     
     cls.default_grids = {
         "phi1": np.arange(*cls.coord_bounds["phi1"], 0.2),
         "phi2": np.arange(*cls.coord_bounds["phi2"], 0.1),
-        "pm1": np.arange(*cls.coord_bounds["pm1"], 0.1),
-        "pm2": np.arange(*cls.coord_bounds["pm2"], 0.1),
+        "pm1": np.arange(*cls.coord_bounds["pm1"], 0.025),
+        "pm2": np.arange(*cls.coord_bounds["pm2"], 0.025),
     }
     return cls
 
@@ -34,6 +34,7 @@ def make_bkg_model(cls, pawprint, data, knot_sep, phi2_bkg=False):
     cls = setup_models(cls, pawprint, data)
 
     cls.phi1_locs = get_grid(*cls.phi1_lim, knot_sep, pad_num=1).reshape(-1, 1)
+    cls.phi2_locs = get_grid(*cls.phi2_lim, knot_sep, pad_num=1).reshape(-1, 1) # works for m92 but knot_sep might not be good in general
     cls.phi2_knots = get_grid(*cls.coord_bounds["phi1"], knot_sep)
     cls.pm1_knots = get_grid(*cls.coord_bounds["phi1"], knot_sep)
     cls.pm2_knots = get_grid(*cls.coord_bounds["phi1"], knot_sep)
@@ -47,16 +48,25 @@ def make_bkg_model(cls, pawprint, data, knot_sep, phi2_bkg=False):
             scales=np.full_like(cls.phi1_locs, 10.0),
             coord_bounds=cls.phi1_lim,
         ),
-        "phi2": (Normal1DSplineVariable(
-                param_priors={
-                    "mean": dist.Uniform(-20,20).expand(cls.phi2_knots.shape),
-                    "ln_std": dist.Uniform(0.5,5).expand(cls.phi2_knots.shape),
-                },
-                knots=cls.phi2_knots,
-                # spline_ks = {"w":1},
-                coord_bounds=cls.coord_bounds["phi2"],
+        "phi2": (GridGMMVariable(
+                    param_priors={
+                        "zs": dist.Uniform(-8.0, 8.0).expand((cls.phi2_locs.shape[0] - 1,)),
+                    },
+                    locs=cls.phi2_locs,
+                    scales=np.full_like(cls.phi2_locs, 10.0),
+                    coord_bounds=cls.phi2_lim,
                 )
             )
+            # (Normal1DSplineVariable(
+            #     param_priors={
+            #         "mean": dist.Uniform(-20,20).expand(cls.phi2_knots.shape),
+            #         "ln_std": dist.Uniform(0.5,5).expand(cls.phi2_knots.shape),
+            #     },
+            #     knots=cls.phi2_knots,
+            #     # spline_ks = {"w":1},
+            #     coord_bounds=cls.coord_bounds["phi2"],
+            #     )
+            # )
             if phi2_bkg else 
                 (UniformVariable(
                     param_priors={}, coord_bounds=cls.coord_bounds["phi2"]
@@ -91,9 +101,10 @@ def make_bkg_model(cls, pawprint, data, knot_sep, phi2_bkg=False):
         cls.data_required = {
         "pm1": {"x": "phi1", "y": "pm1", "y_err": "pm1_err"},
         "pm2": {"x": "phi1", "y": "pm2", "y_err": "pm2_err"},
-        "phi2": {"x": "phi1", "y": "phi2"},
+        # "phi2": {"x": "phi1", "y": "phi2"},
     }
-        cls._data_required['phi2'] = {"x": "phi1", "y": "phi2"}
+        # cls._data_required['phi2'] = {"x": "phi1", "y": "phi2"}
+        cls._data_required['phi2'] = {"y": "phi2", "y_err": "phi2_err"}
     else:
         cls.data_required = {
         "pm1": {"x": "phi1", "y": "pm1", "y_err": "pm1_err"},
